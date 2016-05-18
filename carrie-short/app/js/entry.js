2e6741e2ed417ffa5c28f76bf5bed484d04afc6b
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-17 22:56:02 -0700

    basic to do list without backend

--- carrie-short/app/js/entry.js
+++ carrie-short/app/js/entry.js
@@ -1,2 +1,14 @@
 const angular = require('angular');
 require(__dirname + '/../css/style.css');
+const taskApp = angular.module('taskApp',[]);
+
+require('./directives')(taskApp);
+
+taskApp.run(['$rootScope', function($rs) {
+  $rs.tasks = [
+    { desc: 'some task' },
+    { desc: 'another task' },
+    { desc: 'and another' },
+    { desc: 'and another one' }
+  ];
+}]);
--- carrie-short/gulpfile.js
+++ carrie-short/gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
-const html = require('html-loader');
 
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
@@ -19,6 +18,25 @@ gulp.task('webpack:dev', () => {
     }
   }))
   .pipe(gulp.dest('./build'));
+});
+
+gulp.task('webpack:test', function() {
+  return gulp.src('test/entry.js')
+    .pipe(webpack({
+      devtool: 'source-map',
+      output: {
+        filename: 'bundle.js'
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.html$/,
+            loader: 'html'
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('test/'));
 });
 
 gulp.task('static:dev', () => {
