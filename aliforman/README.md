4d432d842bbf06183a761eda304bad0c85182f48
AlegriaForman <alegriaforman@gmail.com>
2016-05-18 19:58:06 -0700

    add transclude to directives intro

---
+++ aliforman/README.md
@@ -0,0 +1,5 @@
+# Directives Introduction - Add 'transclude'
+  * Type 'npm install' to install dependencies.
+  * Type 'gulp' to run tests and webpack.
+  * Then type 'node server.js' to start the server.
+  * Open your browser and type 'localhost:5000'
--- aliforman/app/js/directives/todo_item_directive.js
+++ aliforman/app/js/directives/todo_item_directive.js
@@ -13,19 +13,34 @@ module.exports = function(app) {
     };
   });
 
+  app.directive('simpleCheckbox', function() {
+    return {
+      scope: true,
+      template: '<div checkbox>check</div>'
+    };
+  });
+
+  app.directive('checkbox', function() {
+    return {
+      scope: true,
+      transclude: true,
+      template: '<div><input type="checkbox"><label ng-transclude></label></div>'
+    };
+  });
+
   app.directive('clickToEdit', function() {
     var editorTemplate = '' +
       '<span class="click-to-edit">' +
-        '<div class="fields" ng-hide="view.editorEnabled">' +
+        '<span class="fields" ng-hide="view.editorEnabled">' +
           '{{todo}} ' +
           '<button ng-click="enableEditor()">Edit</button>' +
-        '</div>' +
-          '<div ng-show="view.editorEnabled">' +
+        '</span>' +
+          '<span ng-show="view.editorEnabled">' +
           '<input type="text" ng-model="view.editableValue">' +
           '<button ng-click="save()">Save</button>' +
           ' or ' +
           '<button ng-click="disableEditor()">Cancel</button>' +
-        '</div>' +
+        '</span>' +
       '</span>';
 
     return {
@@ -35,21 +50,21 @@ module.exports = function(app) {
       scope: {
         value: '=clickToEdit'
       },
-      link: function(scope, element) {
-        scope.view = {
-          editableValue: scope.value,
-          editorEnabled: false
-        };
-        scope.enableEditor = function() {
-          scope.view.editorEnabled = true;
-          scope.view.editableValue = scope.value;
-        };
-        scope.disableEditor = function() {
-          scope.view.editorEnabled = false;
-        };
-        scope.save = function() {
-          scope.value = scope.view.editableValue;
-          scope.disableEditor();
+    link: function(scope, element) {
+      scope.view = {
+        editableValue: scope.value,
+        editorEnabled: false
+      };
+      scope.enableEditor = function() {
+        scope.view.editorEnabled = true;
+        scope.view.editableValue = scope.value;
+      };
+      scope.disableEditor = function() {
+        scope.view.editorEnabled = false;
+      };
+      scope.save = function() {
+        scope.value = scope.view.editableValue;
+        scope.disableEditor();
       };
     }
     };
