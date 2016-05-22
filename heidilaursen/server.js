d5f1c45bc909d5485ea876be26ccef4cc539f0ff
heidilaursen <heidimlaursen@gmail.com>
2016-05-22 13:07:05 -0700

    update server

--- heidilaursen/server.js
+++ heidilaursen/server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/build'))
-  .listen(5000, () => console.log('server Up'));
+  .listen(5000, () => process.stdout.write('server up on 5000'));
